3669221cae54b4c562da151e3331c8c843d279db
Dmytro Manchynskyi <xtrafrancyz@gmail.com>
2026-02-27 05:04:34 +0200

    add loadTexture methods

--- src/main/java/net/glintwein/ui/render/texture/TextureManager.java
+++ src/main/java/net/glintwein/ui/render/texture/TextureManager.java
@@ -3,6 +3,7 @@ package net.glintwein.ui.render.texture;
 import com.mojang.blaze3d.platform.NativeImage;
 import net.glintwein.util.ResourceLoader;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 public class TextureManager {
@@ -17,6 +18,19 @@ public class TextureManager {
             }
         } else {
             throw new UnsupportedOperationException("Unsupported texture format for path: " + path);
+        }
+    }
+
+    public static Texture loadTexture(byte[] data) {
+        return loadTexture(new ByteArrayInputStream(data));
+    }
+
+    public static Texture loadTexture(InputStream is) {
+        try {
+            NativeImage image = NativeImage.read(NativeImage.Format.RGBA, is);
+            return new TextureSimple(image);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load texture from input stream", e);
         }
     }
 }
